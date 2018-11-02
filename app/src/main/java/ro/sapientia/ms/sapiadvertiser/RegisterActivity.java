1c26deacef3d1058f34f2602134987bbef8dd20c
RaymondMagyari <raymond.magyari@gmail.com>
2018-11-02 19:04:36 +0200

    log reg act

--- app/src/main/java/ro/sapientia/ms/sapiadvertiser/RegisterActivity.java
+++ app/src/main/java/ro/sapientia/ms/sapiadvertiser/RegisterActivity.java
@@ -1,4 +1,13 @@
 package ro.sapientia.ms.sapiadvertiser;
 
-public class RegisterActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
 }
