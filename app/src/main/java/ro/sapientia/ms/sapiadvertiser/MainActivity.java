c825ae5d5c75f2385ce2e017e94cd355c9c06fc3
RaymondMagyari <raymond.magyari@gmail.com>
2018-11-03 15:52:53 +0200

    javitasok #1

--- app/src/main/java/ro/sapientia/ms/sapiadvertiser/MainActivity.java
+++ app/src/main/java/ro/sapientia/ms/sapiadvertiser/MainActivity.java
@@ -6,12 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 2500;
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
         new Handler().postDelayed(new Runnable() {
             @Override
