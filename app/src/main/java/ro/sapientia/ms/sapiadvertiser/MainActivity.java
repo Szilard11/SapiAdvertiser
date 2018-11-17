d09ffd3841bbfff766866eb945a8dcfa40c80d1f
Szilard11 <11szilard7@gmail.com>
2018-11-17 17:46:39 +0200

    Phone login update

--- app/src/main/java/ro/sapientia/ms/sapiadvertiser/MainActivity.java
+++ app/src/main/java/ro/sapientia/ms/sapiadvertiser/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(MainActivity.this, NewsActivity.class);
+                Intent homeIntent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(homeIntent);
                 finish();
 
