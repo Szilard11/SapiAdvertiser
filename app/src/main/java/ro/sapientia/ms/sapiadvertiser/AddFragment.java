9c1ab4aaa9361e03310b1801ce3e72bd97e0c8a5
RaymondMagyari <raymond.magyari@gmail.com>
2018-12-22 14:45:29 +0200

    Update #5

--- app/src/main/java/ro/sapientia/ms/sapiadvertiser/AddFragment.java
+++ app/src/main/java/ro/sapientia/ms/sapiadvertiser/AddFragment.java
@@ -1,19 +1,61 @@
 package ro.sapientia.ms.sapiadvertiser;
 
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.Inflater;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AddFragment extends Fragment {
+    private EditText mTitle;
+    private EditText mShortDesc;
+    private EditText mLongDesc;
+    private EditText mPhoneNum;
+    private EditText mLocation;
+    private ImageButton mAddImage;
+    private View mInflatedView;
+    private Button mUpload;
+    private StorageReference mStorageRef;
+    private DatabaseReference mDatabaseRef;
+    private static final int RESULT_LOAD_IMAGE = 1;
+
+
+    private List<String> fileNameList;
+    private List<String> fileDoneList;
+
 
     public AddFragment() {
         // Required empty public constructor
@@ -23,6 +65,116 @@ public class AddFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false);
+
+        this.mInflatedView = inflater.inflate(R.layout.fragment_add, container, false);
+        mTitle = mInflatedView.findViewById(R.id.editText_Title);
+        mShortDesc = mInflatedView.findViewById(R.id.editText_shortDesc);
+        mLongDesc = mInflatedView.findViewById(R.id.editText_longDesc);
+        mPhoneNum = mInflatedView.findViewById(R.id.editText_phone);
+        mLocation = mInflatedView.findViewById(R.id.editText_Location);
+        mAddImage = mInflatedView.findViewById(R.id.imageButton_add);
+        mUpload = mInflatedView.findViewById(R.id.button_Upload);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+        mAddImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"), RESULT_LOAD_IMAGE);
+            }
+        });
+
+        mUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TOdo lecsekkolni a kitoltott mezoket
+                uploadAdv();
+            }
+        });
+
+
+        return this.mInflatedView;
     }
+
+    private void uploadAdv() {
+        //TODO megoldani a feltoltest
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK){
+
+            if(data.getClipData() != null){
+
+                int totalItemsSelected = data.getClipData().getItemCount();
+
+                for(int i = 0; i < totalItemsSelected; i++){
+
+                    Uri fileUri = data.getClipData().getItemAt(i).getUri();
+
+                    String fileName = getFileName(fileUri);
+
+                    fileNameList.add(fileName);
+                    fileDoneList.add("uploading");
+
+
+                    String timestamp = String.valueOf(System.currentTimeMillis());
+                    String fName =  timestamp + String.valueOf(i);
+
+                    StorageReference fileToUpload = mStorageRef.child("Images").child(fName);
+
+                    final int finalI = i;
+                    fileToUpload.putFile(fileUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            fileDoneList.remove(finalI);
+                            fileDoneList.add(finalI, "done");
+                        }
+                    });
+
+                }
+
+                Toast.makeText(getActivity(), "Selected Multiple Files", Toast.LENGTH_SHORT).show();
+
+            } else if (data.getData() != null){
+
+
+                        Toast.makeText(getActivity(), "Selected Single File", Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
+    }
+
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getActivity().getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
+
+
 }
--- app/src/main/java/ro/sapientia/ms/sapiadvertiser/PersonFragment.java
+++ app/src/main/java/ro/sapientia/ms/sapiadvertiser/PersonFragment.java
@@ -3,8 +3,14 @@ package ro.sapientia.ms.sapiadvertiser;
 
 //import android.app.FragmentManager;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -13,8 +19,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,11 +31,19 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -44,6 +61,9 @@ public class PersonFragment extends Fragment {
     private EditText mEmail;
     private EditText mPhoneNr;
     private EditText mAddress;
+    private final int PICK_IMAGE_REQUEST = 71;
+    private Uri filePath;
+    private StorageReference mStorageRef=FirebaseStorage.getInstance().getReference("ProfileImages");
 
     DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("users");
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
@@ -64,8 +84,8 @@ public class PersonFragment extends Fragment {
         mProfileImage = mInflatedView.findViewById(R.id.profile_image);
         mMyAdvs_Button = mInflatedView.findViewById(R.id.advsButt);
         mLogOff_Button = mInflatedView.findViewById(R.id.logoffButt);
-        mSave_Button = mInflatedView.findViewById(R.id.imageButton_Save);
-        mEdit_Button = mInflatedView.findViewById(R.id.imageButton_Edit);
+        mSave_Button = mInflatedView.findViewById(R.id.button_Save);
+        mEdit_Button = mInflatedView.findViewById(R.id.button_Edit);
         mSave_Button.setClickable(false);
         mFName = mInflatedView.findViewById(R.id.editText_FName);
         mLName = mInflatedView.findViewById(R.id.editText_LName);
@@ -117,13 +137,19 @@ public class PersonFragment extends Fragment {
                 fragmentTransaction.commit();
             }
         });
+
+        mProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+
         mLogOff_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //FirebaseAuth auth = FirebaseAuth.getInstance();
-                mAuth.signOut();
-                Intent intent = new Intent(getActivity(),LoginActivity.class);
-                startActivity(intent);
+                LogOffDialog();
             }
         });
         mEdit_Button.setOnClickListener(new View.OnClickListener() {
@@ -131,6 +157,7 @@ public class PersonFragment extends Fragment {
             public void onClick(View view) {
                 mEdit_Button.setClickable(false);
                 mSave_Button.setClickable(true);
+                mProfileImage.setEnabled(true);
                 mFName.setEnabled(true);
                 mLName.setEnabled(true);
                 mPhoneNr.setEnabled(true);
@@ -148,16 +175,111 @@ public class PersonFragment extends Fragment {
                 {
                     mEdit_Button.setClickable(true);
                     mSave_Button.setClickable(false);
+                    mProfileImage.setEnabled(false);
                     mFName.setEnabled(false);
                     mLName.setEnabled(false);
                     mPhoneNr.setEnabled(false);
                     mAddress.setEnabled(false);
                     mEmail.setEnabled(false);
+
                 }
+                uploadImage();
             }
         });
 
         // Inflate the layout for this fragment
         return this.mInflatedView;
+    }
+
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    private void uploadImage() {
+
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(getContext());
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            StorageReference ref = mStorageRef.child(UUID.randomUUID().toString());
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getActivity(), "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getActivity(), "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), filePath);
+                mProfileImage.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void LogOffDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+        builder.setTitle("Sapi Advertiser");
+        builder.setMessage("Are you sure you want to log off?");
+
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                // Do do my action here
+                mAuth.signOut();
+                Intent intent = new Intent(getActivity(),LoginActivity.class);
+                startActivity(intent);
+                dialog.dismiss();
+            }
+
+        });
+
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // I do not need any action here you might
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+
     }
 }
